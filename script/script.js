8f3793241bbb375f685dd0e012763bbc1109e10a
tarikuljisan <tarikuljisan14@gmail.com>
2025-03-04 22:04:48 +0600

    button disabled

--- script/script.js
+++ script/script.js
@@ -6,6 +6,8 @@ document.getElementById('btn').addEventListener('click',function(){
    const newelement = document.createElement('p')
    newelement.innerText='You have completed the task fix mobile button issue at '+newtime;
    Activity.appendChild(newelement)
+   btn.setAttribute('disabled',true)
+   
 
 });
 document.getElementById('btn2').addEventListener('click',function(){
@@ -16,6 +18,7 @@ document.getElementById('btn2').addEventListener('click',function(){
    const newelement = document.createElement('p')
    newelement.innerText='You have completed the task fix mobile button issue at '+newtime;
    Activity.appendChild(newelement)
+   btn2.setAttribute('disabled',true)
 
 });
 document.getElementById('btn3').addEventListener('click',function(){
@@ -26,6 +29,7 @@ document.getElementById('btn3').addEventListener('click',function(){
    const newelement = document.createElement('p')
    newelement.innerText='You have completed the task fix mobile button issue at '+newtime;
    Activity.appendChild(newelement)
+   btn3.setAttribute('disabled',true)
 
 });
 document.getElementById('btn4').addEventListener('click',function(){
@@ -36,6 +40,7 @@ document.getElementById('btn4').addEventListener('click',function(){
    const newelement = document.createElement('p')
    newelement.innerText='You have completed the task fix mobile button issue at '+newtime;
    Activity.appendChild(newelement)
+   btn4.setAttribute('disabled',true)
 
 });
 document.getElementById('btn5').addEventListener('click',function(){
@@ -46,6 +51,7 @@ document.getElementById('btn5').addEventListener('click',function(){
    const newelement = document.createElement('p')
    newelement.innerText='You have completed the task fix mobile button issue at '+newtime;
    Activity.appendChild(newelement)
+   btn5.setAttribute('disabled',true)
 
 });
 document.getElementById('btn6').addEventListener('click',function(){
@@ -57,8 +63,11 @@ document.getElementById('btn6').addEventListener('click',function(){
    newelement.innerText='You have completed the task fix improve job searching at '+newtime;
    Activity.appendChild(newelement)
    alert('Congratulations! You have completed All task')
+   btn6.setAttribute('disabled',true)
 
 });
+
+
 
 
 
